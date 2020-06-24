6aafd8062277f9f682f4d279db011659023f8ff3
Alex <xelanawotka@gmail.com>
2020-06-24 11:50:17 -0400

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var browserSync = require('browser-sync')
 
 var imagemin = require('gulp-imagemin')
 
+
+var ghpages = require('gh-pages')
+
 sass.compiler = require('node-sass')
 
 gulp.task('sass', function() {
@@ -52,6 +55,10 @@ gulp.task('watch', function() {
     gulp.watch('src/css/app.scss', ['sass'])
     gulp.watch('src/fonts/*', ['fonts'])
     gulp.watch('src/img/*', ['images'])
+})
+
+gulp.task('deploy', function() {
+    ghpages.publish("dist")
 })
 
 gulp.task('default', ['html', 'sass', 'fonts', 'images', 'watch'])
